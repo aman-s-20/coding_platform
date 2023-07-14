139050eb87f189246f0640b67cec9e6b3af63df4
Aman Singh <singhaman235136@gmail.com>
2023-07-14 09:13:18 +0530

    added

---
+++ app.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/authRoutes');
+const questionRoutes = require('./routes/questionRoutes');
+const solutionRoutes = require('./routes/solutionRoutes');
+
+app.use(bodyParser.json());
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
+
+// Routes
+app.use('/auth', authRoutes);
+app.use('/questions', questionRoutes);
+app.use('/solutions', solutionRoutes);
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
